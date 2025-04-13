ef2c5af3325f49cfce70cec1f68b67e1078f50f0
Christian Krutsche <christian.krutsche@gmail.com>
2025-04-13 20:45:23 +0200

    Add Firebase configuration to environment variables

--- src/pages/index.ts
+++
@@ -1,5 +0,0 @@
-export { default as Login } from "./login";
-export { default as Register } from "./register";
-export { default as SongAdd } from "./songAdd";
-export { default as SongEdit } from "./songEdit";
-export { default as SongList } from "./songList";
--- src/types.ts
+++
@@ -1,8 +0,0 @@
-export type Song = {
-  id?: string;
-  number?: number;
-  name: string;
-  checkRequired: boolean;
-  withChords: string;
-  withoutChords: string;
-};
